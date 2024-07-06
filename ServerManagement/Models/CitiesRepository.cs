47931eb9d718a8fc64af43924f16f3b8098de87c
Skeeter Man <Michael_Newman@Yamaha-Motor.com>
2024-07-06 09:59:26 -0500

    Add project files.

---
+++ ServerManagement/Models/CitiesRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace ServerManagement.Models
+{
+    public static class CitiesRepository
+    {
+        private static readonly List<string> Cities = new List<string> {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+
+        public static List<string> GetCities() => Cities;
+    }
+}
