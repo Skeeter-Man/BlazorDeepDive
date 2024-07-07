7497c01af6df49e63fdd1a677713bea99f95ab3c
Skeeter Man <Michael_Newman@Yamaha-Motor.com>
2024-07-07 09:00:24 -0500

    Blazor SSR

--- ServerManagement/Models/Server.cs
+++ ServerManagement/Models/Server.cs
@@ -1,4 +1,6 @@
-﻿namespace ServerManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServerManagement.Models
 {
     public class Server
     {
@@ -10,8 +12,13 @@
         }
 
         public int Id { get; set; }
+
         public bool IsOnline { get; set; }
+
+        [Required]
         public string? Name { get; set; }
+
+        [Required]
         public string? City { get; set; }
     }
 }
